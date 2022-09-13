cf4bc45cbaa413a7abc80f5a3d6d8f067f6d1038
Santiago Alzamora <santialzamora_6@hotmail.com>
2022-09-13 15:48:09 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const cors = require('cors')
+const {Server} = require('socket.io')
+
+app.use(cors())
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors:{
+        origin:"*",
+        methods:["GET","POST"]
+    }
+})
+
+io.on("connection",(socket)=>{
+    console.log("User connected");
+
+    socket.on("joinRoom",(roomId)=>{
+        socket.join(roomId)
+        console.log("User joined to the room: ",roomId);
+    })
+
+    socket.on("sendMessage",(data)=>{
+        socket.to(data.room).emit("receiveMessage",data)
+    })
+
+    socket.on("disconnect",()=>{
+        console.log("disconnected");
+    })
+})
+
+const PORT = process.env.PORT || 3001
+
+server.listen(PORT,()=>{
+    console.log("Server running on port ",PORT);
+})
